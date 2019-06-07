3e38d568b64e42f059cc2b9e29858f7f8eb34b94
chrissuthy <chrissuthy@gmail.com>
2019-06-07 10:02:54 -0400

    added some post-proces plotting

--- example.R
+++ example.R
@@ -19,9 +19,26 @@ mapview(wmnf) +
   mapview(wmnf.ss, cex=0.5)
 
 
+# parameters
+ntraps <- 50
+
+
 #get the optimal design:
 design50 <- SCRdesign(statespace = st_coordinates(wmnf.ss)/1000,
                       all.traps = st_coordinates(wmnf.traps)/1000,
-                      ntraps = 50, nn = 30, ndesigns = 20,
+                      ntraps = ntraps, nn = 30, ndesigns = 3,
                       sigma = 0.75, beta0 = 0.05, crit = 6)
 
+opt.traps1 <- st_as_sf(data.frame(design50$Xlst[[1]]*1000,
+                       coords = c("X","Y"), crs = crs(wmnf)))
+opt.traps2 <- st_as_sf(data.frame(design50$Xlst[[2]]*1000,
+                       coords = c("X","Y"), crs = crs(wmnf)))
+opt.traps3 <- st_as_sf(data.frame(design50$Xlst[[3]]*1000),
+                       coords = c("X","Y"), crs = crs(wmnf))
+
+mapview(wmnf) +
+  mapview(wmnf.traps, cex=0.1, color=NA) +
+  mapview(wmnf.ss, cex=0.5) + 
+  mapview(opt.traps1, col.region="red") +
+  mapview(opt.traps2, col.region="blue") +
+  mapview(opt.traps3, col.region="green")
